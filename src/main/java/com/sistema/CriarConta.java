a67e80adb0afe6b04d1150842d0fcbd4c94f4e2c
XDeathFoxX <valdemirvoltatoni@gmail.com>
2025-04-15 21:39:29 -0300

    change

---
+++ src/main/java/com/sistema/CriarConta.java
@@ -0,0 +1,19 @@
+package com.sistema;
+
+public class CriarConta {
+    private int numConta;
+    private String nomeConta;
+    private String senhaConta;
+    private String cpfConta;
+    private String cnpjConta;
+
+    public CriarConta() {
+        this.numConta = 0;
+        this.nomeConta = "";
+        this.senhaConta = "";
+        this.cpfConta = "";
+        this.cnpjConta = "";
+    }
+
+    
+}
