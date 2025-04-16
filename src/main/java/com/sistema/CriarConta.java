4b091897f08afdbb5f5c9a503257d09eeccb8998
XDeathFoxX <valdemirvoltatoni@gmail.com>
2025-04-15 21:48:52 -0300

    change

--- src/main/java/com/sistema/CriarConta.java
+++ src/main/java/com/sistema/CriarConta.java
@@ -1,11 +1,12 @@
 package com.sistema;
 
-public class CriarConta {
+public class CriarConta implements ContaPf {
     private int numConta;
     private String nomeConta;
     private String senhaConta;
     private String cpfConta;
     private String cnpjConta;
+    private float saldoConta;
 
     public CriarConta() {
         this.numConta = 0;
@@ -13,7 +14,74 @@ public class CriarConta {
         this.senhaConta = "";
         this.cpfConta = "";
         this.cnpjConta = "";
+        this.saldoConta = 0;
     }
+
+    public float getSaldoConta() {
+        return saldoConta;
+    }
+
+    public void setSaldoConta(float saldoConta) {
+        this.saldoConta = saldoConta;
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getNomeConta() {
+        return nomeConta;
+    }
+
+    public void setNomeConta(String nomeConta) {
+        this.nomeConta = nomeConta;
+    }
+
+    public String getSenhaConta() {
+        return senhaConta;
+    }
+
+    public void setSenhaConta(String senhaConta) {
+        this.senhaConta = senhaConta;
+    }
+
+    public String getCpfConta() {
+        return cpfConta;
+    }
+
+    public void setCpfConta(String cpfConta) {
+        this.cpfConta = cpfConta;
+    }
+
+    public String getCnpjConta() {
+        return cnpjConta;
+    }
+
+    public void setCnpjConta(String cnpjConta) {
+        this.cnpjConta = cnpjConta;
+    }
+
+    @Override
+    public void verificarSaldo() {
+        
+    }
+
+    @Override
+    public void sacarSaldo() {
+        
+    }
+
+    @Override
+    public void transferirSaldo() {
+        
+    }
+
+    
+
 
     
 }
